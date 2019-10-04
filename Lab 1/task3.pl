920847e50d0c02bdd42f255c934758cd2601375f
Emil Ståhl <emil.stahl@icloud.com>
2019-10-04 17:33:29 +0200

    task3 now works properly

--- Lab 1/task3.pl
+++ Lab 1/task3.pl
@@ -1,9 +1,4 @@
 
-%findlast([Rest],[],Rest).
-%findlast([_|Rest],[_|X],LastLine):-
- % findlast(Rest,X,LastLine).
-
-
-
-findLast([H], [], H).
-findLast([H|T], [H|R], Last) :- findLast(T, R, Last).
+findlast([H],[], H).
+findlast([H|T],[H|Rest],LastLine):-
+  findlast(T, Rest,LastLine).
--- Lab 1/task5.pl
+++ Lab 1/task5.pl
@@ -1,5 +1,9 @@
+%permute([],[]).
+%permute([H|T],Y):-
+  %permute(T,L),
+  %select(H,Y,L).
 
-permute([],[]).
-permute([H|T],Y):-
-  permute(T,L),
-  select(H,Y,L).
+permute([], []).
+permute(In, [X|R]) :-
+    select(X, In, Rest),
+    permute(Rest, R).
