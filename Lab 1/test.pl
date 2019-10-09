2fa81d9ccb7ed67f9caafcd531f17e6b74e51ba9
Emil Ståhl <emil.stahl@icloud.com>
2019-10-09 12:33:42 +0200

    Update test.pl

--- Lab 1/test.pl
+++ Lab 1/test.pl
@@ -7,5 +7,5 @@
 app([],X,X).
 app([X|L1],L2,[X|L3]):-
         app(L1,L2,L3).
-%select(X,[X|T],T).
-%select(X,[Y|T],[Y|R]) :- select(X,T,R).
+select(X,[X|T],T).
+select(X,[Y|T],[Y|R]) :- select(X,T,R).
