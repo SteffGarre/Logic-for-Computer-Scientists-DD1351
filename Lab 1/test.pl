e6135b7c7f1f06fa2249ffcc3eb7fd3c86492ab5
Emil Ståhl <emil.stahl@icloud.com>
2019-10-09 12:29:06 +0200

    Update test.pl

--- Lab 1/test.pl
+++ Lab 1/test.pl
@@ -4,8 +4,8 @@
 %append(X, Y, P), 
 %append(X, [H|Y], S).
 
-%app([],X,X).
-%app([X|L1],L2,[X|L3]):-
-  %      app(L1,L2,L3).
-select(X,[X|T],T).
-select(X,[Y|T],[Y|R]) :- select(X,T,R).
+app([],X,X).
+app([X|L1],L2,[X|L3]):-
+        app(L1,L2,L3).
+%select(X,[X|T],T).
+%select(X,[Y|T],[Y|R]) :- select(X,T,R).
